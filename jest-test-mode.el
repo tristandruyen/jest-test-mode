f9ae3a530757b6285292c114787bd307ea818e2c
Tristan Druyen <tristandruyen@vault81.de>
2022-03-23 13:23:16 +0100

    Add alternative keybinds

--- jest-test-mode.el
+++ jest-test-mode.el
@@ -94,6 +94,18 @@ Placeholders are:
 
 (defvar jest-test-mode-map
   (let ((map (make-sparse-keymap)))
+    (define-key map (kbd "m t p")   'jest-test-run-all-tests)
+    (define-key map (kbd "m t C-p") 'jest-test-run-all-tests)
+    (define-key map (kbd "m t n")   'jest-test-run)
+    (define-key map (kbd "m t C-n") 'jest-test-run)
+    (define-key map (kbd "m t a")   'jest-test-rerun-test)
+    (define-key map (kbd "m t C-a") 'jest-test-rerun-test)
+    (define-key map (kbd "m t t")   'jest-test-run-at-point)
+    (define-key map (kbd "m t C-t") 'jest-test-run-at-point)
+    (define-key map (kbd "m t d n") 'jest-test-debug)
+    (define-key map (kbd "m t d a") 'jest-test-debug-rerun-test)
+    (define-key map (kbd "m t d t") 'jest-test-debug-run-at-point)
+
     (define-key map (kbd "C-c C-t p")   'jest-test-run-all-tests)
     (define-key map (kbd "C-c C-t C-p") 'jest-test-run-all-tests)
     (define-key map (kbd "C-c C-t n")   'jest-test-run)
